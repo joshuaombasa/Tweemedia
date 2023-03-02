f02888f76d671a8ae5270e1043fd8a8e93fad248
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-03-02 10:18:07 +0300

    updated scripting

--- index.js
+++ index.js
@@ -1,16 +1,46 @@
 import { tweetsData } from './data.js'
+// import { v4 as uuidv4 } from 'uuid';
 
 const container = document.getElementById("container")
+const postTextAreaInput = document.getElementById("post-text-area")
+const tweetBtn  =document.getElementById("tweet-btn")
 let targetObj = []
+let eventId = ''
+
+tweetBtn.addEventListener('click', createNewPost)
+
+function createNewPost() {
+    const newUserData = {
+        handle: `@EngOMBASA ✅`,
+        profilePic: `images/ombasa.jpg`,
+        likes: 67,
+        retweets: 4,
+        tweetText: postTextAreaInput.value,
+        replies:[],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '123456th5j6ujuyj56rttrj'
+    }
+    tweetsData.unshift(newUserData)
+    postTextAreaInput.value = ''
+    
+    render()
+}
+
 container.addEventListener('click', (e) => {
+    eventId = e.target.id
     targetObj = tweetsData.filter((tweet) => {
         if (tweet.uuid === e.target.id) {
             return tweet
         }
     })[0]
     
-    handleLikes()
-    handleRetweets()
+    if (e.target.dataset.retweet === "retweet") {
+        handleRetweets()
+    } else if (e.target.dataset.like === "heart") {
+        handleLikes()
+    }
+    
     
     render()
 })
@@ -18,6 +48,8 @@ container.addEventListener('click', (e) => {
 function handleLikes() {
     if (!targetObj.isLiked) {
         targetObj.likes  ++
+        document.querySelector('[data-like="heart"]').classList.add("liked")
+        console.log(document.querySelector('[data-like="heart"]').classList)
     } else {
         targetObj.likes  -- 
     }
@@ -31,7 +63,7 @@ function handleRetweets() {
         targetObj.retweets ++
 
     } else {
-        targetObj.retweets ++
+        targetObj.retweets --
         
     }
 
@@ -48,9 +80,11 @@ function render() {
                 <p class="user-name" id="user-name">${tweet.handle}</p>
                 <p class="user-post" id="user-post">${tweet.tweetText}</p>
                 <div class="post-stats" id="post-stats">
-                      <i class="fa-solid fa-comment stats-text-styling" id="${tweet.uuid}">${tweet.replies.length}</i>
-                      <i class="fa-solid fa-heart stats-text-styling" id="${tweet.uuid}">${tweet.likes}</i>
-                      <i class="fa-solid fa-retweet stats-text-styling" id="${tweet.uuid}">${tweet.retweets}</i>
+
+                      <P class="stats-text-styling"><i class="fa-solid fa-comment"></i>${tweet.replies.length}</P>
+                      <P class="stats-text-styling"><i class="fa-solid fa-heart" id="${tweet.uuid}" data-like="heart"></i>${tweet.likes}</p>
+                      <p class="stats-text-styling"><i class="fa-solid fa-retweet" data-retweet="retweet" id="${tweet.uuid}"></i>${tweet.retweets}</p>
+                      
                 </div>
             </div>
         </div>
